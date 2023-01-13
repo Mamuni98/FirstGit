68a754ad6c448cdb27e39d319ef6c1ab6d0489e7
Mamuni98 <nabanitasahoo468@gmail.com>
2023-01-13 22:25:49 +0530

    dom-manipulation-1

---
+++ DOM/task3.js
@@ -0,0 +1,23 @@
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.domain);
+//console.log(document.title);
+//document.title="hello";
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all[10]);
+//document.all[10].textContent= "hello";
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+//var headTitle= document.getElementById('header-title');
+//console.log(headTitle.innerText);
+//console.log(headTitle.textContent);
+var header = document.getElementById("main-header");
+header.style.borderBottom="3px solid black";
+var title= document.getElementsByClassName("title");
+//console.log(title);
+title[0].style.color="green";
+title[0].style.fontWeight="bold";
+
