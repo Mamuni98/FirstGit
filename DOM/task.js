6a68387dead8217eac7994c0d7a1c27e366d7f70
Mamuni98 <nabanitasahoo468@gmail.com>
2023-01-14 20:53:21 +0530

    task-4

---
+++ DOM/task.js
@@ -0,0 +1,29 @@
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.domain);
+//console.log(document.title);
+//document.title="hello";
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all[10]);
+//document.all[10].textContent= "hello";
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+//var headTitle= document.getElementById('header-title');
+//console.log(headTitle.innerText);
+//console.log(headTitle.textContent);
+/*var header = document.getElementById("main-header");
+header.style.borderBottom="3px solid black";
+var title= document.getElementsByClassName("title");
+//console.log(title);
+title[0].style.color="green";
+title[0].style.fontWeight="bold";*/
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = 'green';
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
+
